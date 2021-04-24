5a703cde08f9b99c00a2318c80fd6fa5b17b8ebd
Joby-Sunny <jobys39@live.com>
2021-04-24 16:33:35 +0530

    Static view for World-Wide Done

--- src/app/Routes/worldwide/worldwide.component.ts
+++ src/app/Routes/worldwide/worldwide.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UIRoutes } from 'src/app/Constants/uiroutes';
 
 @Component({
   selector: 'app-worldwide',
   templateUrl: './worldwide.component.html',
-  styleUrls: ['./worldwide.component.scss']
+  styleUrls: ['./worldwide.component.scss'],
 })
 export class WorldwideComponent implements OnInit {
+  public avatarInfo: { title: string; icon: string; subTitle: string } = {
+    title: 'World Wide',
+    icon: 'assets/world-icon.png',
+    subTitle: 'Global Covid-19 Infomation',
+  };
+  public previousPage: { path: string; label: string } = {
+    path: `/${UIRoutes.COUNTRIES}`,
+    label: 'Previous : Top Affected Countries',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
